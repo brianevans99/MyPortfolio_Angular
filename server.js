4c3a99484e173605dd0143eb560d7cfec9766218
Brian Evans <brianevans99@gmail.com>
2020-12-04 09:55:34 -0500

    updated server.js and package.json scripts

--- server.js
+++ server.js
@@ -3,23 +3,10 @@ const path = require('path')
 
 const app = express()
 
-//initialize middleware
-app.use(
-  express.json({
-    extended: false,
-  })
-)
+app.use(express.static(__dirname + '/client/dist/myapp'))
 
-//Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  //set static folder
-  app.use(express.static('client/build'))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/dist/myapp/index.html'))
+})
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(process.env.PORT || 8080)
