098e46058381447c263beae73ee34fe85a28483c
Brian Evans <brianevans99@gmail.com>
2020-12-04 15:29:44 -0500

    updated font awesome social icons

--- src/app/components/social/social-list/social-list.component.ts
+++ src/app/components/social/social-list/social-list.component.ts
@@ -9,23 +9,23 @@ export class SocialListComponent implements OnInit {
   icons: Icon[] = [
     new Icon(
       "https://www.github.com/brianevans99",
-      "text-yellow-600",
-      "github"
+      "text-yellow-600 hover:text-yellow-800",
+      "fab fa-github-alt"
     ),
     new Icon(
       "https://www.linkedin.com/in/brianevans99",
-      "text-blue-600",
-      "linkedin"
+      "text-blue-600 hover:text-blue-800",
+      "fab fa-linkedin-in"
     ),
     new Icon(
       "https://www.instagram.com/brian_evans99",
-      "text-pink-600",
-      "instagram"
+      "text-pink-600 hover:text-pink-800",
+      "fab fa-instagram"
     ),
     new Icon(
       "https://www.facebook.com/brianevans99",
-      "text-blue-800",
-      "facebook"
+      "text-blue-800 hover:text-blue-900",
+      "fab fa-facebook-square"
     ),
   ];
 
