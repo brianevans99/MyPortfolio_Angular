5f7d6dd267e5600aa345ab5c8047a01888d8e9d9
Brian Evans <brianevans99@gmail.com>
2020-12-04 15:17:45 -0500

    revised class names for social icons in header component

--- src/app/components/social/social-list/social-list.component.ts
+++ src/app/components/social/social-list/social-list.component.ts
@@ -8,23 +8,23 @@ import { Icon } from "../social.model";
 export class SocialListComponent implements OnInit {
   icons: Icon[] = [
     new Icon(
-      "https://www.linkedin.com/in/brianevans99",
-      "text-blue-600 hover:text-blue-700",
-      "linkedin"
-    ),
-    new Icon(
       "https://www.github.com/brianevans99",
-      "text-yellow-600 hover:text-yellow-700",
+      "text-yellow-600",
       "github"
     ),
     new Icon(
+      "https://www.linkedin.com/in/brianevans99",
+      "text-blue-600",
+      "linkedin"
+    ),
+    new Icon(
       "https://www.instagram.com/brian_evans99",
-      "text-pink-600 hover:text-pink-700",
+      "text-pink-600",
       "instagram"
     ),
     new Icon(
       "https://www.facebook.com/brianevans99",
-      "text-blue-800 hover:text-blue-900",
+      "text-blue-800",
       "facebook"
     ),
   ];
